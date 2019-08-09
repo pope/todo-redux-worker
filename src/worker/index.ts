e417da80284d1a186c190ed8d7a464c94fe6b9d2
K. Adam Christensen <pope@google.com>
2019-08-09 13:12:24 -0700

    Declare `self` as a Worker instead of creating a new `context` variable.

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,12 +1,12 @@
 import { store } from './state';
 
-const context: Worker = self as any;
+declare var self: Worker;
 
-context.addEventListener('message', (ev) => {
-    switch(ev.data) {
+self.addEventListener('message', (ev) => {
+    switch (ev.data) {
         case 'INCREMENT':
         case 'DECREMENT':
-            store.dispatch({ type: ev.data });
+            store.dispatch({type: ev.data});
             break;
         default:
             console.warn(`Unhandled message: ${ev.data}`);
