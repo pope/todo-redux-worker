b8b57280ab79fd86b824c3a536d18f1d191ca855
K. Adam Christensen <pope@shifteleven.com>
2019-08-11 09:29:35 -0700

    Switched to lit-html

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,17 +1,26 @@
 import 'todomvc-app-css/index.css';
 
+import { render } from 'lit-html';
 import { addTodo } from '../shared/actions';
 import { assert } from '../shared/asserts';
+import { TodoState } from '../shared/types';
+import { appTemplate } from './components/app';
 import { dispatch, subscribe } from './store';
+
+const body = assert(document.querySelector('body'));
+body.classList.remove('loading');
+
+const appEl = assert(document.querySelector('#app'));
+
+function renderApp(state: TodoState): void {
+    render(appTemplate(state), appEl);
+}
 
 subscribe({
     next(state) {
-        console.log(state);
+        renderApp(state);
     },
 });
 
 dispatch(addTodo('Do a thing'));
 dispatch(addTodo('Do another thing'));
-
-const body = assert(document.querySelector('body'));
-body.classList.remove('loading');
