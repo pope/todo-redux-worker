c42f1b8ad57fb7d1b67b3dccbad6709265836319
K. Adam Christensen <pope@shifteleven.com>
2019-08-11 15:59:48 -0700

    Create the worker in index.html
    Browsers do not preload `as=worker` yet, so let's cheat by creating the
    worker early in index.html to speed things up.

--- src/ui/store.ts
+++ src/ui/store.ts
@@ -2,17 +2,21 @@ import { Action } from 'redux';
 import { PartialObserver, ReplaySubject, Subscription } from 'rxjs';
 import { TodoState } from '../shared/types';
 
-const worker = new Worker('/dist/worker.bundle.js');
+declare global {
+    interface Window {
+        todoWorker: Worker;
+    }
+}
 
 const currentState$ = new ReplaySubject<TodoState>();
 
-worker.addEventListener('message', ev => {
+self.todoWorker.addEventListener('message', ev => {
     currentState$.next(ev.data);
 });
 
 /** Dispatches an action to the Redux store. */
 export function dispatch(action: Action): void {
-    worker.postMessage(action);
+    self.todoWorker.postMessage(action);
 }
 
 /** Subscribes to changes on the TodoState from the Redux store. */
