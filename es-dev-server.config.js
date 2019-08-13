4970e4478be9b83a93534ca438e82cf30cb53221
K. Adam Christensen <pope@shifteleven.com>
2019-08-12 22:05:28 -0700

    Use typescript2 plugin.Also cleaned up es-dev-server.config to embrace the rollup life.Of note - `compress` was preventing the JS changes from reloading thebrowser.

--- es-dev-server.config.js
+++ es-dev-server.config.js
@@ -1,26 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-const bodyparser = require('koa-bodyparser');
-const compress = require('koa-compress');
-
 module.exports = {
-    middlewares: [
-        bodyparser(),
-        compress(),
-        async function replacer(context, next) {
-            await next();
-            if (!/redux/.test(context.request.url) || !context.response.body) {
-                return;
-            }
-            const body = context.response.body.replace(
-                /process\.env\.NODE_ENV/g,
-                JSON.stringify('production')
-            );
-            context.body = body;
-        },
-    ],
-    nodeResolve: true,
+    nodeResolve: false,
     open: false,
     port: 8080,
+    rootDir: 'dist',
     watch: true,
 };
